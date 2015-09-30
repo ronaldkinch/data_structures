e31e9e1f1d98327f6e929d785eac3324109d9502
Ronald Kinch <ronaldkinch@comcast.net>
2015-09-29 23:05:50 -0700

    Linked list class closes #1

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,5 +1,75 @@
-def method
-  true
+class Node
+  attr_accessor(:value, :link)
+  def initialize(value = nil)
+    @value = value
+    @link = nil
+  end
+
+  def to_s
+    this.inspect
+  end
 end
 
-puts method
+class LinkedList
+  attr_reader(:length)
+  def initialize
+    @head = Node.new(:HEAD)
+    @length = 0
+  end
+
+  def to_s
+    output = []
+    temp = @head
+    until temp.link.nil?
+      output << temp.link.value
+      temp = temp.link
+    end
+    output.join(", ")
+  end
+
+  def insert(node, index = 0)
+    temp = @head
+    index = validate_index(index)
+    index.times do
+      temp = temp.link
+    end
+    node.link = temp.link
+    temp.link = node
+    @length += 1
+    @head
+  end
+
+  def search(desired)
+    temp = @head
+    index = 0
+    @length.times do
+      temp = temp.link
+      return index if temp.value == desired
+      index += 1
+    end
+    nil
+  end
+
+  def remove(desired)
+    temp = @head
+    @length.times do
+      if temp.link.value == desired
+        out_node = temp.link
+        temp.link = out_node.link
+        out_node.link = nil
+        return out_node
+      end
+      temp = temp.link
+      return nil if temp.link.nil?
+    end
+    nil
+  end
+
+  private
+
+  def validate_index(index)
+    index = 0 if index == :HEAD || index == :first
+    index = @length if index == :last
+    0 <= index && index <= @length ? index : @length
+  end
+end
--- lib/run_linked_list.rb
+++ lib/run_linked_list.rb
@@ -1,4 +1,20 @@
 require_relative "linked_list.rb"
 
-puts method
-# puts method(ARGV[0])
+list = LinkedList.new
+p list.to_s
+list.insert(Node.new("hello"))
+p list.to_s
+list.insert(Node.new(3), 1)
+p list.to_s
+list.insert(Node.new(:one), :HEAD)
+p list.to_s
+list.insert(Node.new("bye"), :last)
+p list.to_s
+list.insert(Node.new("last"), -1)
+p list.to_s
+p list.search("hello")
+p list.search("byebye")
+p list.remove(3)
+p list.to_s
+p list.remove("byebye")
+p list.to_s
